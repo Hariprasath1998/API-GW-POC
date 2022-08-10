dbfe82bd388bb6232bccbd26092a2b9bf51e699c
Hariprasath Ravichandran <hari@Hariprasaths-MacBook-Pro.local>
2022-08-10 20:58:41 +0530

    test

--- app-deploy/backend.tf
+++ app-deploy/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "gcs" {
     bucket      = "api-gw-poc-state"
     prefix      = "terraform-application/state"
-    credentials = "melodic-splicer-357510-fe2d4c9af51f.json"
+    # credentials = "melodic-splicer-357510-fe2d4c9af51f.json"
   }
 }
