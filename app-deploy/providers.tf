0f9521b33a4b7076a1d7f9346f25125cc57f3d20
Hariprasath Ravichandran <hari@Hariprasaths-MacBook-Pro.local>
2022-08-10 14:25:01 +0530

    SA key

--- app-deploy/providers.tf
+++ app-deploy/providers.tf
@@ -1,7 +1,7 @@
 provider "google" {
   project     = var.project_id
   region      = var.region
-  credentials = file("melodic-splicer-357510-fe2d4c9af51f.json")
+  # credentials = file("melodic-splicer-357510-fe2d4c9af51f.json")
 }
 
 
--- cluster-deploy/providers.tf
+++ cluster-deploy/providers.tf
@@ -1,5 +1,5 @@
 provider "google" {
   project     = var.project_id
   region      = var.region
-  credentials = file("melodic-splicer-357510-fe2d4c9af51f.json")
+  # credentials = file("melodic-splicer-357510-fe2d4c9af51f.json")
 }
