3353f7df765980f2a82be560c2ab305a7545b9d9
Hariprasath Ravichandran <hari@Hariprasaths-MacBook-Pro.local>
2022-08-10 14:30:20 +0530

    private cluster

--- cicd_destroy_script.sh
+++ cicd_destroy_script.sh
@@ -2,12 +2,12 @@
 set -e
 
 
-terraform -chdir=./app-deploy destroy --var-file=./tfvars/dev.tfvars --auto-approve
+terraform -chdir=./cluster-deploy destroy --var-file=<(cat ./cluster-deploy/tfvars/dev.tfvars ./dev_common.tfvars) --auto-approve
 
 sleep 30
 
 
-terraform -chdir=./cluster-deploy destroy --var-file=./tfvars/dev.tfvars --auto-approve
+terraform -chdir=./app-deploy destroy --var-file=<(cat ./app-deploy/tfvars/dev.tfvars ./dev_common.tfvars) --auto-approve
 
 
 
--- cluster-deploy/cluster.tf
+++ cluster-deploy/cluster.tf
@@ -23,7 +23,7 @@ resource "google_container_cluster" "tyk" {
 
   private_cluster_config {
     enable_private_nodes    = true
-    enable_private_endpoint = false
+    enable_private_endpoint = true
     master_ipv4_cidr_block  = "10.0.0.0/28"
   }
 
